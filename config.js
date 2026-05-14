dd483c4b0bffcf7e803738c63fa39106b70f9e17
wiLson <codeless.ar@gmail.com>
2026-05-14 17:57:02 -0300

    chore: update SCRIPT_URL deployment endpoint in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Cargar antes de main.js / admin.js / carga.js en cada HTML.
  */
 window.COMER_CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyMk7-YQF_4Wcec3NolC7lGPxGCHRqkncdzsvUzCrsuAhYwS8T_a-nA7com5kR3VIJ8/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzUduij4x6VBciSgmyBKZzZmrWYQbs-9SqHqmXXJwn5SoZaxJqcHySBlqAcJOqgjstx/exec',
     TRACKING_URL: 'https://script.google.com/macros/s/AKfycbxT0G-CwgaZgAZsKXdNgrMm5Bffl77ItglK_CtJyVYX1_OWeeI7Ze0eue1eO4fsujFw/exec',
     MAIN_DATA_SHEET: 'https://docs.google.com/spreadsheets/d/1x6ZnQFGZW-YkzoCxN51NXvpsYl3XuV4rtfBN5k7EucA/export?format=csv',
     VOTES_SHEET: 'https://docs.google.com/spreadsheets/d/1x6ZnQFGZW-YkzoCxN51NXvpsYl3XuV4rtfBN5k7EucA/export?format=csv&gid=1795075061',
