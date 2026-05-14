f0ed300b621d6c15e9fbb02c86bfb72142d5be68
wiLson <codeless.ar@gmail.com>
2026-05-14 18:04:58 -0300

    chore: update SCRIPT_URL to latest Google Apps Script deployment endpoint

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Cargar antes de main.js / admin.js / carga.js en cada HTML.
  */
 window.COMER_CONFIG = {
-    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzUduij4x6VBciSgmyBKZzZmrWYQbs-9SqHqmXXJwn5SoZaxJqcHySBlqAcJOqgjstx/exec',
+    SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwQpS19Mz9srg2OQHIN-Vl5wVG87thlsjypvBP4Q2tt-FMflzOYd7ayaHPtrSvRobo-/exec',
     TRACKING_URL: 'https://script.google.com/macros/s/AKfycbxT0G-CwgaZgAZsKXdNgrMm5Bffl77ItglK_CtJyVYX1_OWeeI7Ze0eue1eO4fsujFw/exec',
     MAIN_DATA_SHEET: 'https://docs.google.com/spreadsheets/d/1x6ZnQFGZW-YkzoCxN51NXvpsYl3XuV4rtfBN5k7EucA/export?format=csv',
     VOTES_SHEET: 'https://docs.google.com/spreadsheets/d/1x6ZnQFGZW-YkzoCxN51NXvpsYl3XuV4rtfBN5k7EucA/export?format=csv&gid=1795075061',
